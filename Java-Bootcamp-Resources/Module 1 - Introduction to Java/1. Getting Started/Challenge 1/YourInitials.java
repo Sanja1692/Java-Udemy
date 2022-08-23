022c959ee1ce42771a8a0b6c454e47085038f22e
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-08-23 15:25:08 +0200

    first commit

---
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here. 
+        System.out.println(" SSS   K   K");
+        System.out.println("S   S  K  K ");
+        System.out.println("S      K K  ");
+        System.out.println(" SSS   KK   ");
+        System.out.println("    S  K K");
+        System.out.println("S   S  K  K ");
+        System.out.println(" SSS   K   K");
+
+    }
+}
---
+++ Section 01/HelloJava.java
@@ -0,0 +1,11 @@
+public class HelloJava{ //class name must be the same as a files name
+    public static void main(String[] args) {
+        System.out.println("Hello Java"); //println( ) prints each message on a new line.
+
+    }
+
+}
+
+//in terminal
+// javac <file-name>.java  -> compiles the code
+// java file-name runs the code
