edf3c5cbdc45126e35a15c755280e7729e0af8de
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-08-24 22:42:27 +0200

    int long double

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -1,5 +1,14 @@
 public class Biography {
     public static void main(String[] args) {
+        String name = "Sanja";
+        int age = 30;
+        String country = "Austria";
+        String sport = "volleyball";
+        int hours = 2;
+        String game = "biliar";
+        String subject = "biology";
+        int grade = 1;
+
 
         //make a name variable
         //make an age variable
@@ -10,10 +19,10 @@ public class Biography {
         //make a subject variable
         //make a grade variable
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is " + name +". I'm " + age +" years old, and I'm from " + country +".");
+        System.out.println("My favourite sport is " + sport +". I play for " + hours +" hours a day.");
+        System.out.println("When I'm tired, I like to play " + game +".");
+        System.out.println("In school, my favourite subject was " + subject +", I scored a " + grade +".");
 
     }
 }
---
+++ Section 02/Decimals.java
@@ -0,0 +1,13 @@
+public class Decimals {
+    public static void main(String[] args) {
+        double percentage = 55.7;
+        System.out.println("In 2022, " + percentage + "% of the world's pop is urban.");
+int dividend =25;
+int divisor=2;
+System.out.println(dividend/divisor); //12
+
+double dividend1 =25;
+double divisor1=2;
+System.out.println(dividend1 + " divided by " + divisor1 + " = " + dividend1/divisor1); //12.5
+    }
+}
