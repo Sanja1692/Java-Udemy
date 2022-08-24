8c8a6aa2afa774610800b3a253876d5cef57ca53
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-08-24 11:32:19 +0200

    int string char

---
+++ Section 02/Bus.java
@@ -0,0 +1,31 @@
+public class Bus {
+    public static void main(String[] args) {
+        String sentence = "Number of passengers: ";
+        int passengers = 0;
+        passengers += 9; // passengers = passengers + 9; 
+        System.out.println(sentence + passengers);
+        passengers -= 5;
+        System.out.println(sentence + passengers);
+        passengers -= 4;
+        System.out.println(sentence + passengers);
+        System.out.println("End of shift");
+    }
+}
+
+
+// int apples = 50;
+// int goneBad = 5;
+// int rotten = 20;
+// int sold = 20;
+ 
+// apples -= goneBad;
+// apples -= rotten;
+// apples -= sold;
+
+
+// int score = 0;
+// int goal = 1;
+// score += goal;
+// score += goal;
+// score += goal;
+// score += goal;
