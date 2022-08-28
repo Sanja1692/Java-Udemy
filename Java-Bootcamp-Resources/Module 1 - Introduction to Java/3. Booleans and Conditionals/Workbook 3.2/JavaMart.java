77af91c629234e0e10e2990c22fa46c6847b8114
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-08-28 16:04:18 +0200

    if else

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
@@ -10,7 +10,15 @@ public class JavaMart {
             //       pay for the toy car 
 
         //else:  Sorry, I only have <wallet> left.
-
+        if (wallet >= toyCar){
+            System.out.println("Sure!");
+            wallet -= toyCar;//       pay for the toy car
+        }
+       
+else{
+    System.out.println(" Sorry, I only have "+wallet+" left.");
+}
+    
         
         double nike = 95.99;       //run test case with 89.99
         System.out.println("Can I get these nike shoes?");
@@ -19,6 +27,11 @@ public class JavaMart {
             //       pay for the nike shoes 
 
         //else: Sorry, I only have <wallet> left.
+        
+        if (wallet>=nike) {
+            System.out.println("Sure!");
+            wallet -= nike;
+        }else{ System.out.println(" Sorry, I only have "+wallet+" left.");}
 
     }
 }
