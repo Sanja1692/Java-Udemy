4ed0b6dd0350b50fbf71efce3394ee686434a686
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-08-28 16:15:42 +0200

    if else if else

--- Section 03/IfElse.java
+++ Section 03/IfElse.java
@@ -1,11 +1,15 @@
 public class IfElse {
     public static void main(String[] args) {
-        int grade = 45;
+        int grade = 65;
 
-        if(grade >= 50) {
-            System.out.println("Great job, you passed!");
-        }else {
-            System.out.println("Study harder next time!");
-        }
+        if(grade >= 80) {
+            System.out.println("You got an A");
+        }else if (grade >=70){
+            System.out.println("You got an B");
+        }else if (grade >=60){
+            System.out.println("You got an C");
+        }else if (grade >=50){
+            System.out.println("You got an D");
+        }else{System.out.println("You got an F");}
     }
 }
