6bb651bc637f6ea26bc15ef29712b541806f7c85
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-08-28 15:35:23 +0200

    Boolean

---
+++ Section 03/BooleanComparisons.java
@@ -0,0 +1,24 @@
+public class BooleanComparisons {
+    public static void main(String[] args) {
+        int chemistryGrade = 95;
+        int biologyGrade = 75;
+        int englichGrade = 75;
+
+        System.out.println(chemistryGrade >=englichGrade ); //false/true
+        System.out.println(biologyGrade <=englichGrade ); //
+        System.out.println(chemistryGrade == biologyGrade);
+        System.out.println(biologyGrade != englichGrade); //not equal to
+
+        //to compare String values
+        String sentence = "I love this course!";
+        String sentence2 = "I love this course!";
+        System.out.println(sentence.equals(sentence2));
+        System.out.println(!sentence.equals(sentence2));//not equal to
+
+
+
+        double sales = 37.55;
+        double costs = 5.55;
+        System.out.println( sales < costs);
+    }
+}
