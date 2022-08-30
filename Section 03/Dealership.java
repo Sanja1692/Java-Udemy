6aab2942777535721273eafa42fc232e950ef684
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-08-30 15:38:11 +0200

    not finish

--- Section 03/Dealership.java
+++ Section 03/Dealership.java
@@ -9,7 +9,15 @@ public class Dealership {
 
         String option = scanner.nextLine();
         switch (option) {
-            case "a": System.out.println("you chose option " + option);break;
+            case "a": System.out.println("What is your budget?");
+            int budget = scanner.nextInt();
+            if (budget >= 10000){
+                System.out.println("Great! A nissan Altima is available.");
+                System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
+            }else{
+                System.out.println("We don't sell cars under $10000. Sorry!");
+            }break;
+
             case "b": System.out.println("you chose option " + option);break;
             default: System.out.println("Invalid option");
 scanner.close();
