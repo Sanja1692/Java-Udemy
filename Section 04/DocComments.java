f69b136af6da08568355a7cb3bcbbcfacd11bc55
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-01 22:58:18 +0200

    Doc Comments

---
+++ Section 04/DocComments.java
@@ -0,0 +1,43 @@
+public class DocComments {
+    public static void main(String[] args) {
+        
+    }
+    /**
+     * Function nam. greete
+     * 
+     * Inside the function:
+     * 1. prints: 'Hello'
+     */
+    public static void greet() {
+    System.out.println("Hello");
+    }
+
+    /**
+     * Function name: printText
+     *
+     * @param name(String)
+     * @param age(String)
+     *      
+     * Inside the function:
+     * 1. prints the name and age as part of a text
+     */
+    public static void printText(String name, String age) {
+        System.out.println("Hi, I'm  " + name + " and I'm  " + age+" years old.");
+    }
+
+    /**
+     * Function name: calculateArea
+     * 
+     * @param length(double)
+     * @param width(double)
+     * @return
+     * Inside the function:
+     * 1. calculate the area and return it
+     * 
+     */
+    public static double calculateArea(double length, double width){
+    double area = length * width;
+    return area;
+    }
+
+}
