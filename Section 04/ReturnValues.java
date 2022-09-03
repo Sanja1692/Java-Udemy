c4babf7cb8a2ea72e57d67c384d5cb28f28f13da
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-03 15:45:34 +0200

    option

--- Section 04/ReturnValues.java
+++ Section 04/ReturnValues.java
@@ -1,24 +1,27 @@
 public class ReturnValues {
     public static void main(String[] args) {
-        double measure1 = measureRectangle(2.2,4.3);
-        double measure2 = measureRectangle(3.2,4.1);
-        double measure3 = measureRectangle(1.5,4.5);
-        double measure4 = measureRectangle(4.2,4.0);
-        double measure5 = measureRectangle(20.2,4.7);
-        stringPrinter(2.2, 4.3, measure1);
-        stringPrinter(3.2, 4.1, measure2);
-        stringPrinter(1.5, 4.5, measure3);
-        stringPrinter(4.2, 4.0, measure4);
-        stringPrinter(20.2, 4.7, measure5);
+        double measure1 = measureRectangle(2.2,4.3, "area");
+        double measure2 = measureRectangle(3.2,4.1, "perimeter");
+        double measure3 = measureRectangle(1.5,4.5, "area");
+        double measure4 = measureRectangle(4.2,4.0, "area");
+        double measure5 = measureRectangle(20.2,4.7, "perimeter");
+        stringPrinter(2.2, 4.3, measure1, "area");
+        stringPrinter(3.2, 4.1, measure2, "perimeter");
+        stringPrinter(1.5, 4.5, measure3, "area");
+        stringPrinter(4.2, 4.0, measure4, "area");
+        stringPrinter(20.2, 4.7, measure5, "perimeter");
 
     
     }
-    public static double measureRectangle(double length, double width){
-        double area = width * length;
-        return area;
+    public static double measureRectangle(double length, double width, String option){
+        switch (option){
+        case "area": return width * length;
+        case "perimeter" : return 2*(length+width);
+        default: return 404;
+        }
     }
-    public static void stringPrinter(double length, double width, double area){
-System.out.println("the area of the rectangle with length " + length + " and width " + width + " is equal to " + area + "\n");
+    public static void stringPrinter(double length, double width, double area, String option){
+System.out.println("the "+option+" of the rectangle with length " + length + " and width " + width + " is equal to " + area + "\n");
     }
     
 }
