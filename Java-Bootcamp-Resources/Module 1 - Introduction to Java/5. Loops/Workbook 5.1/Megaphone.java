fde48c3e60d1b12765e1de5855c85d6763c200fb
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-09 00:38:12 +0200

    for loop

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.1/Megaphone.java
@@ -1,6 +1,8 @@
 public class Megaphone {
     public static void main(String[] args) {
-        
+        for(int i=0; i <10; i++) {
+            System.out.println(i+1 +" If Java was easy. they would call it Python!");
+        }
         //Task 1: make a for loop that prints this 10 times:
         //        If Java was easy, they would call it Python!
 
