b72b5e99810e3c780c2fb8121ba60e58be1523b7
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-09 00:19:22 +0200

    loop

---
+++ Section 05/ForLoops.java
@@ -0,0 +1,7 @@
+public class ForLoops {
+    public static void main(String[] args) {
+        for ( int i = 1; i <=3; i++){  // use the FOR LOOP when you know HOW MANY times do you need your code to runs
+            System.err.println(i);
+        }
+    }
+}
