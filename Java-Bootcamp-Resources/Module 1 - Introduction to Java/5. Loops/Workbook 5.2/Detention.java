8c902e23d8a5facb8b0f72de13e38dc510ac7a4d
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-09 00:49:46 +0200

    count

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
         //Task 1 – Scan for the nextLine(), and print it 99 times. 
-        
+        String exemple = scan.nextLine();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i+1 + " " + exemple);}
     }
 }
--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -4,14 +4,16 @@ public class CountingTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
+        System.out.print("Hi Timmy! Choose a number to count to: ");
+        Integer number = scan.nextInt();
         /*Task 1 – Choose a number
 
             Ask: Hi Timmy! Choose a number to count to: 
 
             Make sure Timmy can enter the value BESIDE the question. 
         */
-
+        for (int i = 0; i <= number; i++) {
+            System.out.print(i +" ");}
         /* Task 2 – Count from 0 to that number
 
              Let's say Timmy entered 5, you would display: 0 1 2 3 4 5 
