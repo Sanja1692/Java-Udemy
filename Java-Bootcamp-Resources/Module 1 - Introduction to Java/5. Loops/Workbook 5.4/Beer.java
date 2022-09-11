1408a906af896c34b240c235c913bde423fbfec3
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-11 23:03:03 +0200

    99 bottles

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
@@ -1,7 +1,10 @@
 public class Beer {
     public static void main(String[] args) {
        // Task 2: make a for loop that starts at i = 99, and finishes at i=1.
+       for (int i = 99; i > 1; i--) {
+         sing(i);
     }
+   }
 
     /** Task 1
      * Function name – sing.
@@ -14,5 +17,6 @@ public class Beer {
      *      take one down, pass it around, <i-1> bottles of beer on the wall!
      */
     public static void sing(int i) {
+      System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer! Take one down, pass it around, "+(i-1)+" bottles of beer on the wall!");
     }
 }
