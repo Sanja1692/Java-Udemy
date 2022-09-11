9d9d35600d0c9814129b7b1bb3b7a77e1af5fdfc
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-11 23:31:30 +0200

    FizzBuzz

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.7/FizzBuzz.java
@@ -1,6 +1,21 @@
 public class FizzBuzz {
     public static void main(String[] args) {
 
+for (int i = 0; i <19; i++) {
+    
+    if(i%3 == 0 && i%5 == 0){
+        System.out.println(i+" FizzBuzz");
+    } else if(i%3 == 0){
+    System.out.println(i+" Fizz");
+    }else if(i%5 == 0){
+        System.out.println(i+" Buzz");
+    }else{
+        System.out.println(i);
+    } 
+            
+    }
+
+   }
        // Task 1 – Make a for loop that counts from 0 to 18. 
        
        /* Task 2
@@ -19,4 +34,4 @@ public class FizzBuzz {
 
     }
 
-}
+
