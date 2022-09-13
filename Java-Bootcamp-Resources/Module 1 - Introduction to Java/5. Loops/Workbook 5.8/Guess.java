9496a0ee3144c919dfe3b51ad6e539ab1d2aba73
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-13 23:11:40 +0200

    g

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.8/Guess.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
 public class Guess  {
-    public static void main(String[] args) {
+    public static void main(String[] args) {      
+        Scanner scan = new Scanner(System.in);
 
+        int secret = 2;
+       // System.out.println("Enter a guerss number between 1 and 5.");
        /* Task 1 
             1. Store a "secret" number between 1 and 5. 
             2. Then, prompt the user to enter a guess.
        */
        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
         
-       Scanner scan = new Scanner(System.in);
 
-
+while 
         /* Task 2 
             1. Set up a loop that keeps running while the user is incorrect
                Every time they get it wrong, write 'Guess again: '
