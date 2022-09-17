f4183950d072f29f492b2ccf8939ba8b237ed97a
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-17 07:52:51 +0200

    d

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
@@ -1,17 +1,21 @@
 public class Doubles {
     public static void main(String[] args) {
-
+        rollDice();
+        int dice1;
+        rollDice();
         /* Task 2
             1. Call the rollDice() function twice.
             2. Store the return values in dice1 and dice2.   
-
         */
-        int dice1;
         int dice2;
 
    //      System.out.println("Dice 1: " + dice1);
    //      System.out.println("Dice 2: " + dice2 + "\n");
-
+        while (dice1 != dice2){
+        System.out.println("Dice 1: " + dice1);
+        System.out.println("Dice 2: " + dice2 + "\n");
+        }
+        System.out.println("You rolled doubles!");
         /* Task 3
             1. Set up a loop that keeps running while the two dice aren't the same.
             2. During each run, re-roll the dice and print the two values. 
@@ -21,9 +25,10 @@ public class Doubles {
         // Task 4: After they roll doubles, print: You rolled doubles!
     }
 
-    public static void rollDice(){
-        int randomNumber = Math.random() * 7;
-        return randomNumber;
+    public static int rollDice(){
+        double randomNumber = Math.random() * 6;
+        randomNumber+=1;
+        return (int)randomNumber;
     }
     /** Task 1 
      * Function name: rollDice – rolls a dice between 1 and 6
