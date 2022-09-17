8323f4f23c792d6a3546d6ec21103defba1e76b9
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-17 23:06:29 +0200

    roll the dice

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
@@ -1,17 +1,18 @@
 public class Doubles {
     public static void main(String[] args) {
-        rollDice();
-        int dice1;
-        rollDice();
         /* Task 2
             1. Call the rollDice() function twice.
             2. Store the return values in dice1 and dice2.   
-        */
-        int dice2;
+        */        
+        int dice1 = rollDice();
+        int dice2 = rollDice();
+;
 
    //      System.out.println("Dice 1: " + dice1);
    //      System.out.println("Dice 2: " + dice2 + "\n");
         while (dice1 != dice2){
+            dice1 = rollDice();
+            dice2 = rollDice();
         System.out.println("Dice 1: " + dice1);
         System.out.println("Dice 2: " + dice2 + "\n");
         }
