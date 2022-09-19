c2e080c9fee9a9a4e36d73a049e97cb4b9f07e48
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-19 14:22:14 +0200

    neesting

--- Section 05/NestedLoops.java
+++ Section 05/NestedLoops.java
@@ -1,5 +1,10 @@
 public class NestedLoops {
     public static void main(String[] args) {
-        
+        for (int i = 0; i <3; i++) { // 3passes*3inner loop=9 print statment
+        System.out.println("Run: " + i);
+            for (int j = 0; j < 3; j++) {   
+            System.out.println(j +" Hello");
+        }
+        }
     }
 }
