6c49f5e2baccdc3031ff5c9529a3dcdaead0e762
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-22 01:01:42 +0200

    hhh

---
+++ Section 06/ArraysAndLoops.java
@@ -0,0 +1,8 @@
+public class ArraysAndLoops {
+    public static void main(String[] args) {
+    String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "E A"};
+        for (int i = 0; i < 4; i++) {
+            System.out.println("The element at index " + i+ " is " + kingdoms[i]);
+    }
+}
+}
