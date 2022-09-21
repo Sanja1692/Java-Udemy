8b4fcd38717466793f8a5813b01643ddf316e53b
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-21 11:51:00 +0200

    a

--- Section 06/IntroToArrays.java
+++ Section 06/IntroToArrays.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class IntroToArrays {
 public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    
+    String[] kingdoms = {"Mercia", "Wessex", "Northumbria", "E A"};
+    System.out.println(kingdoms[0]);//access an element with index
+    System.out.println(kingdoms[1]);
+    System.out.println(kingdoms[2]);
+    System.out.println(kingdoms[3]);
+    // System.out.println(kingdoms[4]); error doesnt exist
+    // System.out.println(kingdoms); error - just reference number
+
+
 }    
 }
