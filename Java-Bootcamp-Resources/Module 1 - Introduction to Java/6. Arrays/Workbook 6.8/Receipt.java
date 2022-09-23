8a981be6c88ba8e40ee090e8b5126d3e56779ff6
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-23 22:12:33 +0200

    sd

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.8/Receipt.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.8/Receipt.java
@@ -4,7 +4,8 @@ public class Receipt {
     public static void main(String[] args) {
 
         //Task 1: Create two arrays (see Workbook article)
-
+        String[] apples = {"Gala", "Granny Smith","Macintosh"};
+        double[] price = {1.99, 1.49, 1.29};
         System.out.println("Here's your receipt:\n");
         /* Task 2
             Use a for loop to print each apple and the corresponding price.
