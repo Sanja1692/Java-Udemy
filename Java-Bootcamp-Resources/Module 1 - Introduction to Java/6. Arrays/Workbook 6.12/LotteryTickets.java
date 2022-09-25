34b9170055393a180164891e7932cbfe6fba2b0c
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-25 21:57:21 +0200

    print it

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.12/LotteryTickets.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.12/LotteryTickets.java
@@ -4,14 +4,16 @@ public class LotteryTickets {
     public static void main(String[] args) {
         int[] ticket = {34, 43, 45, 65, 21, 54};
         //Task 1 - Copy the elements of ticket into ticket2, but change the third value to 54. 
-        int[] ticket2 = new int[ticket.length];
+        int[] ticket2 = Arrays.copyOf(ticket, ticket.length);
         ticket2[2] = 54; 
 
         System.out.print("Ticket 1 numbers: ");
-        //Task 3 - Call printTicketNumbers for int[] ticket.   
+        //Task 3 - Call printTicketNumbers for int[] ticket.  
+        printTicketNumbers(ticket); 
 
         System.out.print("Ticket 2 numbers: ");
         //Task 3 - Call printTicketNumbers for int[] ticket2.   
+        printTicketNumbers(ticket2);
     }
     
     /** Task 2
@@ -24,8 +26,8 @@ public class LotteryTickets {
      *   2.   print a new line   
      */
     public static void printTicketNumbers(int[] ticket) {
-    
-        
+        System.out.print(Arrays.toString(ticket));  //or with for
+        System.out.print("\n");
     }
     }
 
