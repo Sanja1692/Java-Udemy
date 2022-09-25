e6aab91daf53b2811c06bef173f1655f97b9a6e9
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-25 21:48:18 +0200

    dont understand

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.11/Weather.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.11/Weather.java
@@ -1,46 +1,58 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Weather {
     public static void main(String[] args) {
         
-        double[] celsius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
-        double[] fahrenheit = celsiusToFahrenheit();
+        double[] celcius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
         //Task 2, call celsiusToFahrenheit and store the result in the fahrenheit array. 
+        double[] fahrenheit = celciusToFahrenheit(celcius);
 
-        //Task 4, Call printTemperatures for celsius and fahrenheit.
+        //Task 4, Call printTemperatures for celcius and fahrenheit.
+        // printTemperatures(celcius,)
+        // printTemperatures
+        printTemperatures(celcius, "Celsius");
+        printTemperatures(fahrenheit, "Fahrenheit");
     }
 
 
     /** Task 1
      * Function name: celciusToFahrenheit. 
-     * @param celsius ( double[] )
+     * @param celcius ( double[] )
      * @return fahrenheit ( double[] )
      * 
      * Inside the function: 
-     *    1. Create an array 'fahrenheit' with the same length as 'celsius'.
-     *    2. Copy all the values from celsius into the fahrenheit array.
+     *    1. Create an array 'fahrenheit' with the same length as 'celcius'.
+     *    2. Copy all the values from celcius into the fahrenheit array.
      *    3. Update all the values in the array according to: F = (C/5 * 9) + 32.
      *    4. return the fahrenheit array.
      */   
-    public static double celciusToFahrenheit(double[] celsius) {
-        double[] fahrenheit = Arrays.copyOf(celcius,  celsius.length);
-        for (int i = 0; i < fahrenheit.length; i++) {
-        fahrenheit[i] = (celcius[i]/5 *9)+32;
-        return fahrenheit[];
-        }
+    public static double[] celciusToFahrenheit(double[] celcius) {
+        double[] fahrenheit = new double[celcius.length];
+        for (int i = 0; i < celcius.length; i++) {
+            fahrenheit[i] = (celcius[i]/5 *9)+32;
+        }        
+        return fahrenheit;
     }
     /** Task 3
      * Function name - printTemperatures
      * @param temp ( double[] )
-     * @param type ( String ) can be either Celsius or Fahrenheit
+     * @param type ( String ) can be either celcius or Fahrenheit
      * 
      * Inside the function:
      *   1. Uses a for loop to print the temperature values on the SAME line.
-     *       Example:  Celsius: [Celsius values separated by a space]
+     *       Example:  celcius: [celcius values separated by a space]
      *   2. Adds a new line of space after printing the contents of the array.
      *   3. (Task 5) Rounds every temperature value to two decimal places. 
      */
-    public static String printTemperatures(double[] temp, String type) {
+    ///??????????///??????????///??????????///??????????///??????????
+    public static void printTemperatures(double[] temp, String type) {
+        System.out.print(type +": ");
+        for (int i = 0; i < temp.length; i++) {
+            System.out.print(String.format("%.2f", temp[i]) +" ");  ///??????????
+        }
+        System.out.print("\n");
+
     }
 
 }
