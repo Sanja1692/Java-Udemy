392b7a9b4a3565d2463966504d3188e60545ecff
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-26 00:04:41 +0200

    arr

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
@@ -1,7 +1,7 @@
 public class Prices {
     public static void main(String[] args) {
         //Task 1 - Create a 2D array that can store 3 rows and 5 columns of double values.
-
+        double[][] values = new double[3][5];
         // Task 2 - Populate your 2D array with values from the table (see article)
 
         // Task 3 - Print the prices for each department. See the article for the expected output.  
--- Section 06/TDarrays.java
+++ Section 06/TDarrays.java
@@ -37,6 +37,45 @@ public class TDarrays {
         System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3] + " ");
         System.out.println("\tRon: " + grades[1][0] + " " + grades[1][1] + " " +grades[1][2] + " " + grades[1][3] + " ");
         System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3] + " ");
+
+        ////
+        String[][] table3 = new String[3][3]; 
+        System.out.println((table3[0])); // reference
+        ////
+        String[][] table4 = new String[3][3]; 
+        System.out.println((Arrays.toString(table4[0])));// null null null
+        /////
+        String[][] table6 = new String[3][2]; 
+ 
+        System.out.println(table6.length); ///3
+        System.out.println(table6[0].length);//2
+        System.out.println(table6[1].length);//2
+        //
+        String[][] table7 = new String[3][2]; 
+        System.out.println(Arrays.toString(table7[3]));// index out of bounds
+
+        ///
+        String[][] table8 = new String[3][2]; //Each row only has 2 elements. An index of 2 implies there are three.
+
+        System.out.println(table8[2][0]);//null
+        System.out.println(table8[2][2]);//index out of bounds
+        ///
+        int[][] numbers = {
+            {1, 2, 3},
+            {4, 5, 6}
+        }; 
+ 
+        numbers[1][1] = 7; ///{1, 2, 3},
+                     //      {4, 7, 6}
+        ///7
+        int[][] numbers2 = {
+            {1, 2, 3},
+            {4, 5, 6}
+       }; 
+        
+       numbers2[10%2][5%2] = 7;///{1, 7, 3},
+                            //      {4, 7, 6}
+
     }
     
 }
