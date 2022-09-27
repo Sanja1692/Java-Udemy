8e41512b645ddbe4a295813c034df371329e69c1
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-27 22:13:40 +0200

    j

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Javapedia/Javapedia.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Javapedia/Javapedia.java
@@ -7,9 +7,9 @@ public class Javapedia {
         System.out.println("How many historical figures will you register?");
         //Task 1 – Ask the user: how many historical figures will you register?
         //       – Store the value.
-        
+        int figures = scan.nextInt();
         //Task 2 – Create a 2D array with a variable number of rows, and 3 values per row.         
-         
+        String [][] database = new String[figures][3];
         //Watch out for the nextLine() pitfall. 
         /* Task 3 
         for (____) {
@@ -28,11 +28,23 @@ public class Javapedia {
             System.out.print("\n");
 
         }
-        
         */
+        for (int i=0; i< database.length; i++) {
+
+            System.out.println("\n\tFigure " + (i+1)); 
+            System.out.print("\t - Name: ");
+            database[i][0] = scan.nextLine();
+            System.out.print("\t - Date of birth: ");
+            database[i][1] = scan.next();
+            System.out.print("\t - Occupation: ");
+            database[i][2] = scan.next();
+            System.out.print("\n");
+            
+        }
+        
         System.out.println("These are the values you stored:"); 
         //Task 4: call print2DArray. 
-
+        print2DArray(database);
         System.out.print("\nWho do you want information on? ");  
         
         /*Task 5: Let the user search the database by name. 
@@ -56,5 +68,10 @@ public class Javapedia {
      *     • each value in database has one space from the other value. 
      *     • print a new line.
      */
+    public static void print2DArray(int[][] database){
+        System.out.println("\t");
+        System.out.println(" " + [i][j]);
+        System.out.println("\n");
 
+    }
 }
