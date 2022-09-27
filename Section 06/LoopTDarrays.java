bb607c9d045d6257b72960310fe77f3d4c50891b
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-27 14:11:54 +0200

    loop 2d

--- Section 06/LoopTDarrays.java
+++ Section 06/LoopTDarrays.java
@@ -34,6 +34,43 @@ public class LoopTDarrays {
             System.out.print("\n");
 
         }
+        //////
+        int[][] table = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+       }; 
+               
+        // for (int j = 0; j < table.length; j++) {
+        //     System.out.print(table[0][j]);  // 123
+        //     System.out.print(table[1][j]); // 456
+        //     System.out.print("\n");
+
+        // }
+        // for (int i = 0; i < table.length; i++) {
+        //     System.out.print(table[i][0]);  // 147
+        //     System.out.print(table[i][1]); // 258
+        //     System.out.print("\n");
+
+        // }
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                System.out.print(table[i][j]); //////// 123456789
+            System.out.print("\t");
+
+                System.out.print(table[j][i]); /////// 147258369
+            System.out.print("\t");
+
+                System.out.print(table[i][i]);//////111555999
+                System.out.print(table[j][j]);//////159159159
+
+
+            }
+        }
+        //////////////77
+
+
+
     }
     
 }
