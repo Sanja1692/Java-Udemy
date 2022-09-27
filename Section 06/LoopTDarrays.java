edf20e437cd9212fc801a552b361a5bcf63cbdda
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-09-27 13:59:51 +0200

    2D loop

--- Section 06/LoopTDarrays.java
+++ Section 06/LoopTDarrays.java
@@ -19,11 +19,20 @@ public class LoopTDarrays {
         //     System.out.print(grades[2][j] + " ");
         // }
         for (int i = 0; i < grades.length; i++) {
-            System.out.print(grades[0][j] + " ");
+
+        switch(i){
+            case 0: System.out.print("\tHarry: "); break;
+            case 1: System.out.print("\tRon: "); break;
+            case 2: System.out.print("\tHermione: "); break;
+            default: System.out.print("Shouldnt be called");
+
+        }
 
             for (int j = 0; j < grades[i].length; j++) {
-                System.out.print(grades[0][j] + " ");
+                System.out.print(grades[i][j] + " ");
             }
+            System.out.print("\n");
+
         }
     }
     
