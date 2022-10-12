63a435d9d764af8a49d9e265e7953f1ef7fc0d4d
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-10-12 14:54:24 +0200

    fa

--- Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
+++ Java-Bootcamp-Resources/Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
@@ -4,11 +4,11 @@ public class Exams {
         System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
         //Task 1: Create an array that stores 5 students: "Harry", "Neville", "Ron", "Hermione", "Seamus"
         String[] students = {"Harry", "Neville", "Ron", "Hermione", "Seamus"};
-        System.out.println(students[0]+"you will take seat 0.");
-        System.out.println(students[1]+"you will take seat 1.");
-        System.out.println(students[2]+"you will take seat 2.");
-        System.out.println(students[3]+"you will take seat 3.");
-        System.out.println(students[4]+"you will take seat 4.");
+        System.out.println(students[0]+" you will take seat 0.");
+        System.out.println(students[1]+" you will take seat 1.");
+        System.out.println(students[2]+" you will take seat 2.");
+        System.out.println(students[3]+" you will take seat 3.");
+        System.out.println(students[4]+" you will take seat 4.");
 
 
         //Task 2: Assign each student a seat according to their index in the array.
